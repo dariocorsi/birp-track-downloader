9546c7b2385b6ce2e0143eef8cdf254a37aabd4e
Dario Corsi <dariocorsi@gmail.com>
2016-07-01 14:05:27 -0400

    First commit.

---
+++ src/inject/inject.js
@@ -0,0 +1,44 @@
+chrome.extension.sendMessage({}, function(response) {
+    var readyStateCheckInterval = setInterval(function() {
+        if (document.readyState === "complete") {
+            clearInterval(readyStateCheckInterval);
+
+            // ----------------------------------------------------------
+            // This part of the script triggers when page is done loading
+            console.log('BIRP! Track Downloader Loaded');
+            // ----------------------------------------------------------
+
+            var tracks = document.getElementsByClassName('fap-single-track');
+
+            //Get the year and monthbased on current url structure. Should replace with smarter function.
+            var pathname = window.location.pathname.split('/');
+            var year = pathname[2];
+            var month = getMonth(pathname[3]);
+
+            //Get the month from a string
+            function getMonth(str) {
+                var month = str.split('-');
+                return month[0];
+            }
+
+            //Add download link to tracks
+            for (var i = 0; i < tracks.length; ++i) {
+                var element = tracks[i];
+
+                //Make sure the track has a title
+                if (element.getAttribute('title')) {
+                    var trackTitle = element.getAttribute('title');
+                    var trackUrl = trackTitle.replace(/\s/g, '%20');
+                    var downloadLink = document.createElement('a');
+                    downloadLink.href = 'http://www.birp.fm/music/m/playlists/' + year + '/' + month + '-' + year + '/' + trackUrl + '.mp3';
+                    downloadLink.target = '_blank';
+                    downloadLink.download = trackTitle;
+                    downloadLink.innerText = 'Download Track';
+                    downloadLink.style.cssText = 'color: #999; font-size: 12px; margin-left:8px;';
+                    element.parentNode.appendChild(downloadLink);
+                }
+            }
+
+        }
+    }, 10);
+});
