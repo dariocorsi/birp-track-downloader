5754bb54076a5e5e24a2a9c64cffd19f85b14e5b
Dario Corsi <dariocorsi@gmail.com>
2016-07-07 13:51:06 -0400

    Changed sloppy href stuff to base64 decode.

---
+++ readme.md
@@ -0,0 +1,16 @@
+#BIRP! Track Downloader
+
+##About
+
+Adds a 'Download Track' button to single tracks in [BIRP!](www.birp.fm) playlists to replicate old functionality.
+
+##Change Log
+
+###0.0.2
+
+- Added support for *all* playlists
+
+###0.0.1
+
+- Initial release
+- Adds ability to download tracks from June 2016 playlists and newer
--- src/inject/inject.js
+++ src/inject/inject.js
@@ -8,48 +8,48 @@ chrome.extension.sendMessage({}, function(response) {
             console.log('BIRP! Track Downloader Loaded');
             // ----------------------------------------------------------
 
-            var tracks = document.getElementsByClassName('fap-single-track');
-
-            var arr = [].slice.call(tracks);
-
-            var alpha = arr.filter(function(element) {
-                return element.getAttribute('title');
-            }).map(function(element) {
-                return element.innerText.trim();
-            }).sort();
-
-            console.log(alpha);
-
-            //Get the year and month based on current url structure. Should replace with smarter function.
-            var pathname = window.location.pathname.split('/');
-            var year = pathname[2];
-            var month = getMonth(pathname[3]);
-            var playlistName = pathname[3];
-
-            //Get the month from a string
-            function getMonth(str) {
-                var month = str.split('-');
-                return month[0];
-            }
 
             //Add download link to tracks
-            for (var i = 0; i < tracks.length; ++i) {
-                var element = tracks[i];
+            function addDownloadLinks() {
+                //Get all single tracks on page
 
-                //Make sure the track has a title
-                if (element.getAttribute('title')) {
-                    var trackTitle = element.getAttribute('title');
-                    var trackUrl = trackTitle.replace(/\s/g, '%20');
-                    var downloadLink = document.createElement('a');
-                    downloadLink.classList.add('appended-download-link');
-                    downloadLink.href = 'http://www.birp.fm/music/m/playlists/' + year + '/' + month + '-' + year + '/' + trackUrl + '.mp3';
-                    downloadLink.target = '_blank';
-                    downloadLink.download = trackTitle;
-                    downloadLink.innerText = 'Download Track';
-                    downloadLink.style.cssText = 'color: #999; font-size: 12px; margin-left:8px;';
-                    element.parentNode.appendChild(downloadLink);
+                var tracks = document.getElementsByClassName('fap-single-track');
+
+                //Loop through all the tracks
+                for (var i = 0; i < tracks.length; ++i) {
+                    var element = tracks[i];
+
+                    //Make sure the track has a title
+                    if (element.getAttribute('title')) {
+                        var trackTitle = element.getAttribute('title');
+                        var href = element.getAttribute('href');
+                        // HREFs are BASE64 encoded. Decode that shiz.
+                        var decodedHREF = decodeURIComponent(escape(window.atob(href)));
+                        var downloadLink = document.createElement('a');
+                        downloadLink.classList.add('appended-download-link');
+                        downloadLink.href = decodedHREF;
+                        downloadLink.target = '_blank';
+                        downloadLink.download = trackTitle;
+                        downloadLink.innerText = 'Download Track';
+                        downloadLink.style.cssText = 'color: #999; font-size: 12px; margin-left:8px;';
+                        element.parentNode.appendChild(downloadLink);
+                    }
                 }
             }
+
+            //Fire functions on load
+            addDownloadLinks();
+
+            //Do some polling to fix AJAX problem
+            var location = window.location.href;
+            var pollInterval = 1000; //1s
+            var polling = setInterval(function() {
+              //If the href changes
+                if (window.location.href != location) {
+                    location = window.location.href;
+                    addDownloadLinks();
+                }
+            }, pollInterval);
 
         }
     }, 10);
